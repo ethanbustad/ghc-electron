f34bc05450d581b8a8e8d3790f3b3800e275290e
Ethan Bustad <ethan.bustad@liferay.com>
2018-04-02 12:34:54 -0700

    Switch to loading Hangouts Chat instead

--- main.js
+++ main.js
@@ -4,9 +4,6 @@ const app = electron.app
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
 
-const path = require('path')
-const url = require('url')
-
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
@@ -15,12 +12,8 @@ function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600})
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+  // and load the Google Hangouts Chat site
+  mainWindow.loadURL('https://chat.google.com')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
