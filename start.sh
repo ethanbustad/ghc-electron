4fd3606bb946d4db6be8e7798953ef8e19f77aa3
Ethan Bustad <ethan.bustad@liferay.com>
2018-05-02 16:09:25 -0700

    Run install command if dependencies are outdated

--- start.sh
+++ start.sh
@@ -8,9 +8,13 @@ cd $SCRIPTPATH
 
 COMMAND=$(command -v npm >/dev/null 2>&1 && echo "npm" || echo "node")
 
-if [ ! -e "node_modules" ]
+HASH=$(cat npm-shrinkwrap.json | sha256sum | cut -d ' ' -f 1)
+
+if [ ! -e "node_modules" ] || [ ! -e "node_modules/$HASH" ]
 then
 	$COMMAND install
+
+	touch "node_modules/$HASH"
 fi
 
 $COMMAND start
